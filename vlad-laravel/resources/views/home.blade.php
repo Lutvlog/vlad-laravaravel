cfe295d5063b1d7d5bbc335a7d7badb5c31a05a8
Vlad <lutvlog@gmail.com>
2021-08-18 12:09:58 +0400

    added a ./ test commit

--- vlad-laravel/resources/views/home.blade.php
+++ vlad-laravel/resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <div class="body1" style="background-image: url('{{ asset('images/fon1.jpg')}}');">
     <div class="elem">
         <div class="offer">
-            <p class="offer_1" >управляющая компания</p>
+            <p class="offer_1" >управляющая компания.</p>
             <h1 class="offer_2">Лотос Парк</h1>
         </div>
     </div>
